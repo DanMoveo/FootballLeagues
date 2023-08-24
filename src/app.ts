280309800e55c039a1a02789753ba8dc5dfee62b
danmoveo <dang@moveo.co.il>
2023-08-24 13:18:02 +0300

    more orgenize code

--- src/app.ts
+++ src/app.ts
@@ -1,93 +1,114 @@
 import './styles.scss';
-
+const API_BASE_URL = 'https://www.thesportsdb.com/api/v1/json/3';
 const leagueButtonsContainer = document.getElementById('leagueButtons') as HTMLDivElement;
+const teamsContainer = document.querySelector('.teams-container') as HTMLDivElement;
 
 interface League {
   strLeague: string;
   strSport: string;
 }
 
-async function fetchLeagues(): Promise<League[]> {
-  try {
-    const response = await fetch('https://www.thesportsdb.com/api/v1/json/3/all_leagues.php');
-    const data = await response.json();
-    const leagues: League[] = data.leagues;
-    const americanFootballLeagues: League[] = leagues.filter(league => league.strSport === "American Football");
-    return americanFootballLeagues;
-    } catch (error)
-    {console.error('Error fetching leagues:', error);
-    return [];}
-}
-
-async function createLeagueButtons() {
-  const leagues: League[] = await fetchLeagues();
-  const selectedLeagues = leagues.slice(0, 5);
-
-  let activeButton: HTMLButtonElement | null = null; // To store the currently active button
-
-  selectedLeagues.forEach((league: League) => {
-    const button = document.createElement('button');
-    button.className = 'tab';
-    button.setAttribute('data-league', league.strLeague);
-    button.textContent = league.strLeague;
-
-    button.addEventListener('click', () => {
-      const leagueName = button.getAttribute('data-league');
-      if (leagueName) {
-        fetchTeamsForLeague(leagueName);
-
-        // Remove active class from the previously active button
-        if (activeButton) {
-          activeButton.classList.remove('active');
-        }
-
-        // Add active class to the clicked button
-        button.classList.add('active');
-        activeButton = button; // Update the currently active button
-      }
-    });
-
-    leagueButtonsContainer?.appendChild(button);
-  });
-}
-
-createLeagueButtons();
-
-const teamsContainer = document.querySelector('.teams-container') as HTMLDivElement;
-
 interface Team {
   strTeam: string;
   strTeamBadge: string;
 }
 
-async function fetchTeamsForLeague(league: string) {
-  console.log('Fetching teams for league:', league);
-
-  const response = await fetch(`https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=${encodeURIComponent(league)}`);
-  const data = await response.json();
-
-  if (data.teams) {
-    const teams: Team[] = data.teams;
-
-    teamsContainer.innerHTML = '';
-
-    teams.forEach((team: Team) => {
-      const teamElement = document.createElement('div');
-      teamElement.className = 'team';
-
-      const teamLogo = document.createElement('img');
-      teamLogo.className = 'team-logo';
-      teamLogo.src = team.strTeamBadge;
-
-      const teamName = document.createElement('p');
-      teamName.textContent = team.strTeam;
-
-      teamElement.appendChild(teamLogo);
-      teamElement.appendChild(teamName);
-
-      teamsContainer.appendChild(teamElement);
-    });
-  } else {
-    console.log('No teams found for this league.');
+// utility function for fetching data
+async function fetchData<T>(url: string): Promise<T | null> {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
   }
 }
+
+// Fetch leagues by sport
+async function fetchLeagues(sport: string): Promise<League[]> {
+  const url = `${API_BASE_URL}/all_leagues.php`;
+  const data = await fetchData<{ leagues: League[] }>(url);
+  if (!data) {
+    throw new Error('Failed to fetch leagues');
+  }
+  return data.leagues.filter(league => league.strSport === sport);
+}
+
+// Fetch teams for a league
+async function fetchTeamsForLeague(league: string): Promise<Team[]> {
+  const url = `${API_BASE_URL}/search_all_teams.php?l=${encodeURIComponent(league)}`;
+  const data = await fetchData<{ teams: Team[] }>(url);
+  if (!data) {
+    throw new Error('Failed to fetch teams');
+  }
+  return data.teams;
+}
+
+// Display teams on the page
+function displayTeams(teams: Team[]) {
+  const teamsContainer = document.querySelector('.teams-container') as HTMLDivElement;
+  teamsContainer.innerHTML = '';
+
+  teams.forEach(team => {
+    const teamElement = document.createElement('div');
+    teamElement.className = 'team';
+
+    const teamLogo = document.createElement('img');
+    teamLogo.className = 'team-logo';
+    teamLogo.src = team.strTeamBadge;
+
+    const teamName = document.createElement('p');
+    teamName.textContent = team.strTeam;
+
+    teamElement.appendChild(teamLogo);
+    teamElement.appendChild(teamName);
+
+    teamsContainer.appendChild(teamElement);
+  });
+}
+
+// Activate the clicked button and deactivate the previously active button
+function activateButton(button: HTMLButtonElement) {
+  const activeButton = document.querySelector('.tab.active') as HTMLButtonElement;
+  if (activeButton) {
+    activeButton.classList.remove('active');
+  }
+  button.classList.add('active');
+}
+
+// Create league buttons and set up event listeners
+async function createLeagueButtons() {
+  try {
+    const leagues = await fetchLeagues("American Football");
+    const selectedLeagues = leagues.slice(0, 5);
+
+    const leagueButtonsContainer = document.getElementById('leagueButtons') as HTMLDivElement;
+    selectedLeagues.forEach(league => {
+      const button = document.createElement('button');
+      button.className = 'tab';
+      button.setAttribute('data-league', league.strLeague);
+      button.textContent = league.strLeague;
+
+      button.addEventListener('click', async () => {
+        const leagueName = button.getAttribute('data-league');
+        if (leagueName) {
+          try {
+            const teams = await fetchTeamsForLeague(leagueName);
+            displayTeams(teams);
+            activateButton(button);
+          } catch (error) {
+            console.error('Error fetching teams:', error);
+          }
+        }
+      });
+
+      leagueButtonsContainer.appendChild(button);
+    });
+  } catch (error) {
+    console.error('Error fetching leagues:', error);
+  }
+}
+
+// Initialize the application
+createLeagueButtons();
